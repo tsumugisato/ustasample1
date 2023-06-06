468d79cd978715592ddc5dd9798528366e89f598
satoutsumugi <tsumugisato0@gmail.com>
2023-06-06 19:31:23 +0900

    キャラが振動するバグを修正

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
  
 public class GameManager: MonoBehaviour {
  
     //カメラを格納する変数
     public Camera Camera;
-    public Camera subCamera;
+    public Camera SubCamera;
+    public Camera ThirdCamera;
+    public Button CameraButton;
+    public Button SubCameraButton;
+    public Button ThirdCameraButton;
+    // public Camera ThirdCamera;
  
     //キャンバスを格納
     public GameObject Canvas;
@@ -16,7 +22,10 @@ public class GameManager: MonoBehaviour {
     void Start () {
  
         //初めはサブカメラをオフにしておく
-        subCamera.enabled = false;
+        Camera.enabled = true;
+        SubCamera.enabled = false;
+        ThirdCamera.enabled = false;
+      //  ThirdCamera.enabled = false;
     }
      
     // Update is called once per frame
@@ -32,35 +41,68 @@ public class GameManager: MonoBehaviour {
          
     }
  
-    //ボタンを押した時の処理
-    public void PushButton()
+    // //ボタンを押した時の処理
+    // public void PushButton(Button anybutton)
+    // {
+    //     Debug.Log("ボタンを押す");
+    //     //もしサブカメラがオフだったら
+    //     if (!ThirdCamera.enabled)
+    //     {
+    //         Debug.Log("サブカメラつけろ");
+    //         //サブカメラをオンにして
+    //         // subCamera.enabled = true;
+    //         ThirdCamera.enabled = true;
+ 
+    //         //カメラをオフにする
+    //         Camera.enabled = false;
+ 
+    //         //キャンバスを映すカメラをサブカメラオブジェクトにする
+    //         Canvas.GetComponent<Canvas>().worldCamera = ThirdCamera;
+    //     }
+    //     //もしサブカメラがオンだったら
+    //     else
+    //     {
+    //         Debug.Log("サブカメラオフにしろ");
+    //         //サブカメラをオフにして
+    //         ThirdCamera.enabled = true;
+    //        // ThirdCamera.enabled = false;
+ 
+    //         //カメラをオンにする
+    //         Camera.enabled = true;
+ 
+    //         //キャンバスを映すカメラをカメラオブジェクトにする
+    //         Canvas.GetComponent<Canvas>().worldCamera = Camera;
+    //     }
+    // }
+
+     public void OnCameraButtonPress()
     {
-        Debug.Log("ボタンを押す");
-        //もしサブカメラがオフだったら
-        if (!subCamera.enabled)
-        {
-            Debug.Log("サブカメラつけろ");
-            //サブカメラをオンにして
-            subCamera.enabled = true;
- 
-            //カメラをオフにする
-            Camera.enabled = false;
- 
-            //キャンバスを映すカメラをサブカメラオブジェクトにする
-            Canvas.GetComponent<Canvas>().worldCamera = subCamera;
-        }
-        //もしサブカメラがオンだったら
-        else
-        {
-            Debug.Log("サブカメラオフにしろ");
-            //サブカメラをオフにして
-            subCamera.enabled = false;
- 
-            //カメラをオンにする
-            Camera.enabled = true;
- 
-            //キャンバスを映すカメラをカメラオブジェクトにする
-            Canvas.GetComponent<Canvas>().worldCamera = Camera;
-        }
+        Debug.Log("1");
+        Camera.enabled = true;
+        SubCamera.enabled = false;
+        ThirdCamera.enabled = false;
+
+        //キャンバスを映すカメラをサブカメラオブジェクトにする
+        Canvas.GetComponent<Canvas>().worldCamera = Camera;
+    }
+
+    public void OnSubCameraButtonPress()
+    {
+        Debug.Log("2");
+        Camera.enabled = false;
+        SubCamera.enabled = true;
+        ThirdCamera.enabled = false;
+        //キャンバスを映すカメラをサブカメラオブジェクトにする
+        Canvas.GetComponent<Canvas>().worldCamera = SubCamera;
+    }
+
+    public void OnThirdCameraButtonPress()
+    {
+        Debug.Log("3");
+        Camera.enabled = false;
+        SubCamera.enabled = false;
+        ThirdCamera.enabled = true;
+        //キャンバスを映すカメラをサブカメラオブジェクトにする
+        Canvas.GetComponent<Canvas>().worldCamera = ThirdCamera;
     }
 }
--- Assets/Script/ScreenShot.cs
+++ Assets/Script/ScreenShot.cs
@@ -1,60 +1,58 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
-// using System.IO;
-// using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
 
 
-// /// 指定されたカメラの内容をキャプチャするサンプル
+/// 指定されたカメラの内容をキャプチャするサンプル
 
-// public class ScreenShot : MonoBehaviour
-// {
-//     [SerializeField] private Camera _camera;
-//     public Button button;
-//     private int counter = 0;
+public class ScreenShot : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    private int counter = 0;
+    public Button screenButton;
 
-//     //  public Button screenButton;
+     private void Start()
+    {
+        // screenButton.onClick.AddListener(Increment);
+    }
 
-//      private void Start()
-//     {
-//         button.onClick.AddListener(Increment);
-//     }
+    public void SaveScreenShot()
+    {
 
-//     public void SaveScreenShot()
-//     {
+        // カウンターを1ずつ増やす
+        counter++;
 
-//         // カウンターを1ずつ増やす
-//         counter++;
-
-//         // テキスト表示を更新
+        // テキスト表示を更新
     
-//         CaptureScreenShot("Assets/CameraScreenShot.png"+counter.ToString());
-//         Debug.Log("スクリーンショットが保存されました。");
-//     }
+        CaptureScreenShot("Assets/CameraScreenShot.png"+counter.ToString());
+        Debug.Log("スクリーンショットが保存されました。");
+    }
 
-//     // カメラのスクリーンショットを保存する
-//     private void CaptureScreenShot(string filePath)
-//     {
-//         var rt = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 24);
-//         var prev = _camera.targetTexture;
-//         _camera.targetTexture = rt;
-//         _camera.Render();
-//         _camera.targetTexture = prev;
-//         RenderTexture.active = rt;
+    // カメラのスクリーンショットを保存する
+    private void CaptureScreenShot(string filePath)
+    {
+        var rt = new RenderTexture(_camera.pixelWidth, _camera.pixelHeight, 24);
+        var prev = _camera.targetTexture;
+        _camera.targetTexture = rt;
+        _camera.Render();
+        _camera.targetTexture = prev;
+        RenderTexture.active = rt;
         
-//         var screenShot = new Texture2D(
-//             _camera.pixelWidth,
-//             _camera.pixelHeight,
-//             TextureFormat.RGB24,
-//             false);
-//         screenShot.ReadPixels(new Rect(0, 0, screenShot.width, screenShot.height), 0, 0);
-//         screenShot.Apply();
+        var screenShot = new Texture2D(
+            _camera.pixelWidth,
+            _camera.pixelHeight,
+            TextureFormat.RGB24,
+            false);
+        screenShot.ReadPixels(new Rect(0, 0, screenShot.width, screenShot.height), 0, 0);
+        screenShot.Apply();
             
-//         var bytes = screenShot.EncodeToPNG();
-//         Destroy(screenShot);
+        var bytes = screenShot.EncodeToPNG();
+        Destroy(screenShot);
             
-//         File.WriteAllBytes(filePath, bytes);
-//     }
+        File.WriteAllBytes(filePath, bytes);
+    }
 
-
-// }
+}
